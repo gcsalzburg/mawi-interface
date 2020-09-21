c34de9ec8119f5f75c828d67ffae2aa3d7a30248
George Cave <gcbristol@gmail.com>
2020-09-21 22:31:13 +0200

    Add drag effects, add, remove, scale movement

--- templates.js
+++ templates.js
@@ -8,8 +8,17 @@ const template_step = `
 		<div class="other-data">
 			<span class="duration_value">{{duration}}</span>
 		</div>
+		<a href="#delete" class="delete_step">Remove</a>
+		<a href="#drag_right" class="step_drag drag_right"></a>
 	</div>
 `;
 
 const template_step_divider = `<div class="step_divider">
 <a href="#add" class="add_step"></a></div>`;
+
+const data_default = {
+	start_value: 0,
+	ease_value: "linear",
+	end_value: 100,
+	duration: 500
+};
--- ui.js
+++ ui.js
@@ -11,13 +11,11 @@ var sequence = {
 					duration: 1000
 				},
 				{
-					start_value: 60,
 					ease_value: "ease-in",
 					end_value: 110,
 					duration: 300
 				},
 				{
-					start_value: 60,
 					ease_value: "bounce-in",
 					end_value: 110,
 					duration: 300
@@ -45,7 +43,6 @@ var sequence = {
 					duration: 250
 				},
 				{
-					start_value: 60,
 					ease_value: "bounce-out",
 					end_value: 110,
 					duration: 300
@@ -88,26 +85,38 @@ function format_joints(){
 		if(joints[i].querySelectorAll('.step').length < 1){
 			joints[i].classList.add('joint_empty');
 		}
-	}
 
+		const steps = joints[i].querySelectorAll('.step');
+		let last_start = -1;
+		for(j=0; j<steps.length; j++){
+			_step = steps[j];
 
-	const steps = document.querySelectorAll('.step');
-	for(i=0; i<steps.length; i++){
-		_step = steps[i];
-		// Set width of block based on duration
-		_step.style.width = (scale_factor * _step.querySelector('.duration_value').textContent) + 'px';
+			// Set width of block based on duration
+			_new_width = (scale_factor * _step.querySelector('.duration_value').textContent);
+			_step.style.width = _new_width + 'px';
+			_step.classList.remove('small');
+			if(_new_width < 200){
+				_step.classList.add("small");
+			}
 
-		// Set colour style based on ease tyle
-		_ease = _step.querySelector('.ease_value').textContent;
-		_step.classList.remove('step_linear');
-		_step.classList.remove('step_ease');
-		_step.classList.remove('step_bounce');
-		if(_ease.indexOf('linear') >= 0){
-			_step.classList.add('step_linear');
-		}else if(_ease.indexOf('ease') >= 0){
-			_step.classList.add('step_ease');
-		}else if(_ease.indexOf('bounce') >= 0){
-			_step.classList.add('step_bounce');
+			// Update start value based on previous start value
+			if(last_start >= 0){
+				_step.querySelector(".start_value").textContent = last_start;
+			}
+			last_start = _step.querySelector(".end_value").textContent;
+
+			// Set colour style based on ease tyle
+			_ease = _step.querySelector('.ease_value').textContent;
+			_step.classList.remove('step_linear');
+			_step.classList.remove('step_ease');
+			_step.classList.remove('step_bounce');
+			if(_ease.indexOf('linear') >= 0){
+				_step.classList.add('step_linear');
+			}else if(_ease.indexOf('ease') >= 0){
+				_step.classList.add('step_ease');
+			}else if(_ease.indexOf('bounce') >= 0){
+				_step.classList.add('step_bounce');
+			}
 		}
 	}
 }
@@ -121,4 +130,159 @@ function build_scale(){
 	for(i=0; i<20; i++){
 		scale.innerHTML += `<span class="major" data-value="${i*100}" style="left:${i*100}px"></span>`;
 	}
+}
+
+// General click handling for UI
+document.onclick = function(event) {
+
+	event.preventDefault();
+
+	const _e = event.target;
+
+	if(_e.classList.contains("add_step")){
+		// Add Step buttons = insert new divider + new step
+		let nodes = document.createRange().createContextualFragment(Mustache.render(template_step, data_default) + Mustache.render(template_step_divider));
+		_e.parentNode.after(nodes);
+	}else	if(_e.classList.contains("delete_step")){
+		// Remove Step buttons = delete step and node after it (divider)
+		_e.parentNode.nextElementSibling.remove();
+		_e.parentNode.remove();
+	}else if(_e.classList.contains("ease_value")){
+		// Select a different ease value
+		// TODO: Create a selector for this
+		if(_e.parentNode.parentNode.classList.contains("step_linear")){
+			_e.textContent = "ease-in";
+		}else if(_e.parentNode.parentNode.classList.contains("step_ease")){
+			_e.textContent = "bounce";
+		}else if(_e.parentNode.parentNode.classList.contains("step_bounce")){
+			_e.textContent = "linear";
+		}
+	}
+	format_joints();
+};
+
+// Drag handling for sliders
+document.ondragstart = function(event){
+	return false;	
+};
+document.onmousedown = function(event){
+
+	const _e = event.target;
+
+	if(_e.classList.contains("step_divider")){
+		// Drag a step divider
+
+		if(_e.previousElementSibling == null){
+			// If this is the first step divider, we can't drag it
+			return false;
+		}
+		_e.classList.add("is_dragging");
+
+		// Save current sizes and positions
+		const drag_data = {
+			has_before: false,
+			has_after: false
+		};
+		drag_data.start_x = event.pageX;
+		if(_e.previousElementSibling != null){
+			drag_data.has_before = true;
+			drag_data.before_step = _e.previousElementSibling;
+			drag_data.before_w = _e.previousElementSibling.clientWidth;
+		}
+		if(_e.nextElementSibling != null){
+			drag_data.has_after = true;
+			drag_data.after_step = _e.nextElementSibling;
+			drag_data.after_w = _e.nextElementSibling.clientWidth;
+		}
+		function onStepDividerDrag(event){
+			setDividerWidths(drag_data,event.pageX);
+		}
+		document.addEventListener('mousemove', onStepDividerDrag);
+
+		// Remove un-needed handlers on release
+		document.onmouseup = function() {
+			_e.classList.remove("is_dragging");
+			document.removeEventListener('mousemove', onStepDividerDrag);
+			document.onmouseup = null;
+			format_joints();
+		};
+	}else if(_e.classList.contains("step_drag")){
+		// Drag a handle inside an individual step
+
+		_e.classList.add("is_dragging");
+		_e.parentNode.parentNode.classList.add("hide_add_buttons");
+
+		// Save current sizes and positions
+		const drag_data = {
+			has_before: false,
+			has_after: false
+		};
+		drag_data.start_x = event.pageX;
+		if(_e.classList.contains("drag_right")){
+			drag_data.has_before = true;
+			drag_data.before_step = _e.parentNode;
+			drag_data.before_w = _e.parentNode.clientWidth;
+		}
+		function onStepDividerDrag(event){
+			setDividerWidths(drag_data,event.pageX);
+		}
+		document.addEventListener('mousemove', onStepDividerDrag);
+
+		// Remove un-needed handlers on release
+		document.onmouseup = function() {
+			_e.classList.remove("is_dragging");
+			_e.parentNode.parentNode.classList.remove("hide_add_buttons");
+			document.removeEventListener('mousemove', onStepDividerDrag);
+			document.onmouseup = null;
+			format_joints();
+		};
+	}else if(_e.classList.contains("scale")){ // TODO: Allow dragging on the check markers and the numbers as well
+		// Dragging the scale
+		_e.classList.add("is_dragging");
+
+		const drag_data = {
+			start_x: event.pageX
+		};
+		function onScaleDrag(event){
+			_new_x = _e.dataset.offset - (drag_data.start_x - event.pageX);
+			if(_new_x > 0){
+				_new_x = 0;
+			}
+			_e.style.transform = `translateX(${_new_x}px`;
+			_e.dataset.new_offset = _new_x;
+			update_step_offsets(_new_x);
+		}
+		document.addEventListener('mousemove', onScaleDrag);
+
+		// Remove un-needed handlers on release
+		document.onmouseup = function() {
+			_e.classList.remove("is_dragging");
+			_e.dataset.offset = _e.dataset.new_offset;
+			document.removeEventListener('mousemove', onScaleDrag);
+			document.onmouseup = null;
+			format_joints();
+		};
+
+	}
+}
+
+function update_step_offsets(_x){
+	_steps = document.querySelectorAll(".step, .step_divider");
+	for(let i=0; i<_steps.length; i++){
+		_steps[i].style.transform = `translateX(${_x}px`;
+	}
+}
+
+function setDividerWidths(data,new_x){
+	if(data.before_step != null){
+		const new_before_width = Math.max(data.before_w+(new_x-data.start_x),50);
+		data.before_step.style.width = new_before_width+'px';
+		data.before_step.querySelector(".duration_value").textContent = new_before_width;
+	}
+	if(data.after_step != null){
+		const new_after_width = Math.max(data.after_w-(new_x-data.start_x),50);
+		data.after_step.style.width = new_after_width+'px';
+		data.after_step.querySelector(".duration_value").textContent = new_after_width;
+	}
+	format_joints(); // on the fly, so that we apply things like "small" class to shrinking boxes
 }
